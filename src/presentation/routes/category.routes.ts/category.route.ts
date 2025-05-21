ad8ec2a0b0f3cc7ee4c8de2b57fea3cf9fa731a7
laith-alskaf <liethmhmad231@gmail.com>
2025-05-21 14:56:42 +0300

    Add checkResourceOwnership Middleware => CategoryRouers Delete and Put

--- src/presentation/routes/category.routes.ts/category.route.ts
+++ src/presentation/routes/category.routes.ts/category.route.ts
@@ -5,12 +5,14 @@ import {
     validateCategoryId,
     validateUpdateCategory
 } from '../../validation/category.validators';
+import { CategoryModel } from '../../../infrastructure/database/mongodb/models/category.model';
+import { checkResourceOwnership } from '../../middleware/resource-ownership.middleware';
 
 
 
 const categoryRouters = (categoryController: CategoryController): Router => {
     const router = Router();
-
+    const idKey = 'categoryId';
     router.post("/",
         validateCategory,
         categoryController.createCategory.bind(categoryController)
@@ -18,10 +20,12 @@ const categoryRouters = (categoryController: CategoryController): Router => {
 
     router.delete("/:categoryId",
         validateCategoryId,
+        checkResourceOwnership(CategoryModel, idKey),
         categoryController.deleteCategory.bind(categoryController)
     );
     router.put("/:categoryId",
         validateUpdateCategory,
+        checkResourceOwnership(CategoryModel, idKey),
         categoryController.updateCategory.bind(categoryController)
     );
 
